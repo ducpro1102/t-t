12a88ed4eceb53f333036030602da1e013695aab
Admin <Admin@DESKTOP-B200SEA>
2020-10-24 13:51:14 +0700

    Add project files.

---
+++ AngularJSAuthentication/Controllers/OrdersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace AngularJSAuthentication.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        //[Authorize(Roles = "Customer")]
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            //var userId = RequestContext.Principal.Identity.GetUserId();
+            var claimsIdentity = (ClaimsIdentity)RequestContext.Principal.Identity;
+            string xxx = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "sub").Value;
+            
+            return Ok(Order.CreateOrders());
+        }
+
+    }
+
+    #region Helpers
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerName { get; set; }
+        public string ShipperCity { get; set; }
+        public Boolean IsShipped { get; set; }
+
+        public static List<Order> CreateOrders()
+        {
+            List<Order> OrderList = new List<Order>
+            {
+                new Order {OrderID = 10248, CustomerName = "Taiseer Joudeh", ShipperCity = "Amman", IsShipped = true },
+                new Order {OrderID = 10249, CustomerName = "Ahmad Hasan", ShipperCity = "Dubai", IsShipped = false},
+                new Order {OrderID = 10250,CustomerName = "Tamer Yaser", ShipperCity = "Jeddah", IsShipped = false },
+                new Order {OrderID = 10251,CustomerName = "Lina Majed", ShipperCity = "Abu Dhabi", IsShipped = false},
+                new Order {OrderID = 10252,CustomerName = "Yasmeen Rami", ShipperCity = "Kuwait", IsShipped = true}
+            };
+
+            return OrderList;
+        }
+    }
+
+    #endregion
+}
